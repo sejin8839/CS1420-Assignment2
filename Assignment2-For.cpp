5b57dd1b58b4f6432134786dca219bf9a97994f2
sejin8839 <sejinon01@gmail.com>
2023-03-22 19:19:14 +0900

    Create Assignment2-For.cpp

---
+++ Assignment2-For.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    if (num <= 0)
+    {
+        cout << "Please enter a number greater than 0" << endl;
+        return 0;
+    }
+
+    
+    for (;;)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        if (sum == 1)
+        {
+            cout << "The number is a happy number." << endl;
+            break;
+        }
+        else if (sum == 4)
+        {
+            cout << "The number is not a happy number." << endl;
+            break;
+        }
+        else
+        {
+            num = sum;
+        }
+    }
+    return 0;
+}
